97c7999c73cb749294faee440ed062376a4c183c
GollaKrishna123 <gkrishna05348@gmail.com>
2023-05-29 14:15:56 +0530

    first commit

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -6,7 +6,6 @@ const MovieCard = ({ movie}) => {
       <div>
         <p>{movie.Year}</p>
       </div>
-
       <div>
         <img src={movie.Poster !== "N/A" ? movie.Poster : "https://via.placeholder.com/400"} alt={movie.Title} />
       </div>
